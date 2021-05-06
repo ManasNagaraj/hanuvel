706902897dbf73679f3ca9cdb5c9caae1182604c
ManasNagaraj <knmanas6@gmail.com>
2021-05-06 14:19:59 +0530

    inital commit

---
+++ src/components/ContentBlock/index.js
@@ -0,0 +1,12 @@
+import LeftContentBlock from './LeftContentBlock';
+import RightContentBlock from './RightContentBlock';
+import RightContentBlockIntro from './RightContentBlockIntro';
+
+const ContentBlock = (props) => {
+  if (props.type === 'left') return <LeftContentBlock {...props} />;
+  if (props.type === 'right') return <RightContentBlock {...props} />;
+  if (props.type === 'intro') return <RightContentBlockIntro {...props} />;
+  return null;
+};
+
+export default ContentBlock;
