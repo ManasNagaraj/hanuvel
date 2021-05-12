db033e10c94cfce893800611f212ab68f7711270
ManasNagaraj <knmanas6@gmail.com>
2021-05-12 15:38:03 +0530

    banner update

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -92,7 +92,7 @@ const BannerRowTop = ({ title }) => {
         }}
         className='row-col'
       >
-        <span className='row-message'>
+        <span className='row-message' style={{ color: '#ff5200' }}>
           We are specialised in setting up the foundation of your brand and
           setting you up for success.
         </span>
@@ -104,35 +104,6 @@ const BannerRowTop = ({ title }) => {
 const BannerRowBottom = ({ title }) => {
   return (
     <div className={'banner-row center'}>
-      <motion.div
-        initial={{ scale: 0 }}
-        animate={{ scale: 1 }}
-        transition={{ ease: [0.6, 0.01, -0.05, 0.95], duration: 1, delay: 1 }}
-        className='scroll'
-      >
-        <motion.span
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{
-            ease: 'easeInOut',
-            duration: 1,
-            delay: 1.8,
-          }}
-        >
-          scroll
-        </motion.span>
-        <motion.span
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{
-            ease: 'easeInOut',
-            duration: 1,
-            delay: 1.8,
-          }}
-        >
-          down
-        </motion.span>
-      </motion.div>
       <AnimatedLetters title={title} />
     </div>
   );
